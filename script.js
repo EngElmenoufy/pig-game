f93745f358e4b538b015a9049e4e9657f63fec58
EngElmenoufy <engelmenoufy@gmail.com>
2025-07-02 18:02:38 +0300

    Implemented role dice, hold, and new game

--- script.js
+++ script.js
@@ -19,12 +19,18 @@ sub-problems:
   2- put all images and its value to an array of object
 */
 
-const player1 = document.querySelector('.player--0');
-const player2 = document.querySelector('.player--1');
-const totalScore1 = document.querySelector('#score--0');
-const totalScore2 = document.querySelector('#score--1');
-const currentScore1 = document.querySelector('#current--0');
-const currentScore2 = document.querySelector('#current--1');
+const players = [
+  document.querySelector('.player--0'),
+  document.querySelector('.player--1'),
+];
+const totalScores = [
+  document.querySelector('#score--0'),
+  document.querySelector('#score--1'),
+];
+const currentScores = [
+  document.querySelector('#current--0'),
+  document.querySelector('#current--1'),
+];
 const diceImage = document.querySelector('.dice');
 const newGameBtn = document.querySelector('.btn--new');
 const rollBtn = document.querySelector('.btn--roll');
@@ -57,4 +63,61 @@ const dice = [
   },
 ];
 
-let currentRole = null;
+let currentPlayer = 0;
+
+let currentScore = 0;
+
+function roleDice() {
+  const randomRole = Math.trunc(Math.random() * 6);
+  const currentDice = dice[randomRole];
+  diceImage.classList.remove('hidden');
+  diceImage.setAttribute('src', currentDice.imageSrc);
+  if (currentDice.value === 1) {
+    switchPlayer();
+  } else {
+    currentScore += currentDice.value;
+    currentScores[currentPlayer].textContent = currentScore;
+  }
+}
+
+rollBtn.addEventListener('click', roleDice);
+
+function switchPlayer() {
+  currentScore = 0;
+  currentScores[currentPlayer].textContent = '0';
+  players[currentPlayer].classList.remove('player--active');
+  currentPlayer = currentPlayer === 0 ? 1 : 0;
+  players[currentPlayer].classList.add('player--active');
+}
+
+function holdScore() {
+  const oldTotal = Number(totalScores[currentPlayer].textContent);
+  const total = currentScore + oldTotal;
+  totalScores[currentPlayer].textContent = total;
+  currentScores[currentPlayer].textContent = 0;
+  if (total < 100) {
+    switchPlayer();
+  } else {
+    players[currentPlayer].classList.add('player--winner');
+    rollBtn.removeEventListener('click', roleDice);
+    holdBtn.removeEventListener('click', holdScore);
+    diceImage.classList.add('hidden');
+  }
+}
+
+holdBtn.addEventListener('click', holdScore);
+
+function playAgain() {
+  currentPlayer = 0;
+  currentScore = 0;
+  for (let i = 0; i <= 1; i++) {
+    totalScores[i].textContent = 0;
+    currentScores[i].textContent = 0;
+    players[i].classList.remove('player--winner');
+  }
+  diceImage.classList.add('hidden');
+  players[0].classList.add('player--active');
+  players[1].classList.remove('player--active');
+}
+
+newGameBtn.addEventListener('click', playAgain);
